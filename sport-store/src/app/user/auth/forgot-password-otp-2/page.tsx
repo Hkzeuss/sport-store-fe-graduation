7b31ff7121a1b8db50bb741d51b73418c046b439
Hoàng Kiên <126592782+Hkzeuss@users.noreply.github.com>
2025-02-09 20:59:24 +0700

    edit file

--- sport-store/src/app/user/auth/forgot-password-otp-2/page.tsx
+++ sport-store/src/app/user/auth/forgot-password-otp-2/page.tsx
@@ -1,1 +1,16 @@
 // Template nhập otp xác thực để đổi mật khẩu
+// src/app/user/auth/forgot-password-otp-2/page.tsx
+
+'use client';
+
+import React from 'react';
+
+const ForgotPasswordOtpPage = () => {
+  return (
+    <div>
+      <h1>Quên mật khẩu</h1>
+    </div>
+  );
+};
+
+export default ForgotPasswordOtpPage;
